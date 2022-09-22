2ed67147f288bcc331f5e3f3d332d7a971907676
Emmanuel Yañez Martinez <manyzombie@hotmail.com>
2022-09-22 02:15:59 -0500

    Added exersice five

---
+++ src/stories/ExerciseFive/ExerciseFive.stories.js
@@ -0,0 +1,55 @@
+import ExerciseFive from "./ExerciseFive.vue";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+
+export default {
+  title: "ExerciseFive",
+  component: ExerciseFive,
+  argsTypes: {
+    color: { control: "color" },
+    size: { control: { type: "range", min: 1, max: 20, step: 1 } },
+    translateX: { control: { type: "range", min: 1, max: 30, step: 1 } },
+    translateY: { control: { type: "range", min: 1, max: 30, step: 1 } },
+    scaleX: { control: { type: "range", min: 1, max: 30, step: 1 } },
+    scaleY: { control: { type: "range", min: 1, max: 30, step: 1 } },
+  },
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
+    controls: { expanded: true },
+  },
+};
+
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { ExerciseFive },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    // Story args can be spread into the returned object
+    return { ...args };
+  },
+  // Then, the spread values can be accessed directly in the template
+  template: `
+    <ExerciseFive 
+      key='icon' 
+      :color='color' 
+      :size='size' 
+      :position='position' 
+      :translateX='translateX' 
+      :translateY='translateY'
+      :scaleX='scaleX' 
+      :scaleY='scaleY'
+    />
+  `,
+});
+
+export const Desktop = Template.bind({});
+
+Desktop.args = {
+  size: 32,
+  color: "white",
+  translateX: 0,
+  translateY: 0,
+  scaleX: 1,
+  scaleY: 1,
+};
---
+++ src/stories/ExerciseFive/ExerciseFive.vue
@@ -0,0 +1,39 @@
+<template>
+  <span class="v-icon material-icons"> menu </span>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const props = defineProps<{
+  size: number;
+  color: string;
+  translateX: number;
+  translateY: number;
+  scaleX: number;
+  scaleY: number;
+}>();
+
+const iconSize = computed(() => `${props.size}rem`);
+
+const translateIcon = computed(
+  () => `translate(${props.translateX}px,${props.translateY}px)`
+);
+
+const scaleIcon = computed(
+  () => `scaleX(${props.scaleX}) scaleY(${props.scaleY})`
+);
+
+const tranformIcon = computed(
+  () => `${translateIcon.value} ${scaleIcon.value}`
+);
+</script>
+
+<style scoped>
+.v-icon {
+  transition: all 0.2s;
+  font-size: v-bind(iconSize);
+  color: v-bind(color);
+  transform: v-bind(tranformIcon);
+}
+</style>
